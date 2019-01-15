6c0199f2d013eb19effd5e869fcdecbdf93b118a
Peter <peter.tao@mail.utoronto.ca>
2019-01-15 06:15:15 -0500

    change profile

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-// const offset = 85;
-
-// $('.navbar li a').click(function(event) {
-//     event.preventDefault();
-//     $($(this).attr('href'))[0].scrollIntoView({behavior: "smooth"});
-//     // scrollBy(0, -offset);
-
-    
-// });
-
 const offset = 85;
 
 $('.navbar li a').click(function(event) {
     event.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView({behavior: "smooth", block: 'start'});
-    
-
-    
+     
 });
+
+const selfie = document.getElementById('selfie')
+selfie.onmouseover = function(){
+    this.src = "img/selfie.jpg"
+}
+selfie.onmouseout = function(){
+    this.src = "img/selfie2.jpeg"
+}
