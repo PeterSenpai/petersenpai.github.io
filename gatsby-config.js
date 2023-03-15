895423fc8f8f68bd2d95c0bd9d0b8b9efa1481c8
Peter T <petertao9t@gmail.com>
2023-03-14 21:02:38 -0400

    clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,11 @@ module.exports = {
         {
             resolve: "gatsby-theme-portfolio-minimal",
             options: {
-                siteUrl:
-                    "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+                siteUrl: "https://petersenpai.github.io/",
                 manifestSettings: {
-                    favicon: "./content/images/favicon.png", // Path is relative to the root
+                    favicon: "./content/images/favicon.png",
                     siteName: "Peter's Portfolio", // Used in manifest.json
-                    shortName: "Portfolio", // Used in manifest.json
+                    shortName: "Peter Tao", // Used in manifest.json
                     startUrl: "/", // Used in manifest.json
                     backgroundColor: "#FFFFFF", // Used in manifest.json
                     themeColor: "#000000", // Used in manifest.json
@@ -19,11 +18,11 @@ module.exports = {
                     path: "/blog", // Defines the slug for the blog listing page
                     usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
                 },
-                // googleAnalytics: {
-                //     trackingId: "UA-XXXXXX-X",
-                //     anonymize: true, // Default true
-                //     environments: ["production", "development"] // Default ["production"]
-                // }
+                googleAnalytics: {
+                    trackingId: "G-144YH36SVY",
+                    anonymize: false,
+                    environments: ["production"],
+                },
             },
         },
     ],
